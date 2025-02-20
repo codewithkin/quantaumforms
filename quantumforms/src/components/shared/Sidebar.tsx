6be316a038081ec1d08bc1908656c67ece1e74ed
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 23:50:06 +0200

    UPDATE: Add user session handling and profile picture to Sidebar component

--- quantumforms/src/components/shared/Sidebar.tsx
+++ quantumforms/src/components/shared/Sidebar.tsx
@@ -1,18 +1,32 @@
 "use client";
 import React from 'react'
-import { BotMessageSquare, ChartBar, ChartPie, NotebookText, Scale3D, Zap } from 'lucide-react'
+import { BotMessageSquare, ChartBar, ChartPie, DoorOpen, NotebookText, Scale3D, Zap } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import { usePathname } from 'next/navigation';
 import { TooltipTrigger } from '@radix-ui/react-tooltip';
 import { Tooltip, TooltipContent, TooltipProvider } from '../ui/tooltip';
+import { Avatar } from "@heroui/avatar";
+import { auth } from '@/auth';
+import { useQuery } from "@tanstack/react-query";
 
 function Sidebar() {
+    // Get the current path
     const path = usePathname();
 
+    // Get the user's session data
+    const { data: session } = useQuery({
+        initialData: null,
+        queryKey: ["user"],
+        queryFn: async () => await auth()
+    });
+
+    const user = session?.user;
+
+    console.log("USER DATA: ", user);
 
   return (
-    <aside className='md:min-h-screen p-4 bg-white'>
+    <aside className='md:min-h-screen p-4 bg-white flex flex-col items-between'>
         <article className="flex flex-col gap-16">
             {/* App Icon */}
             <article className="bg-orange-500 p-2 rounded-lg flex flex-col justify-center items-center">
@@ -71,6 +85,30 @@ function Sidebar() {
                     </Tooltip>
                 </TooltipProvider>
             </article>
+        </article>
+
+        <article className="flex flex-col gap-4 items-center">
+            <TooltipProvider delayDuration={0.5}>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <Button size="icon" variant="outline" asChild>
+                            <Link href="/auth/signout">
+                                <DoorOpen fill="yellow" size={20} strokeWidth={1.5} />
+                            </Link>
+                        </Button>
+                    </TooltipTrigger>
+
+                    <TooltipContent side="right">Sign Out</TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+
+            {/* User profile picture */}
+            <Avatar
+                name={user?.name}
+                src={user?.image}
+                isBordered
+                size="sm"
+            />
         </article>
     </aside>
   )
