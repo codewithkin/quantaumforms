d43a658533101fcdd6bfea0eac9e6853877990ad
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 10:11:40 +0200

    [ADD]: Nextauth route handler

---
+++ quantumforms/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import AppleProvider from "next-auth/providers/apple"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    AppleProvider({
+      clientId: process.env.APPLE_ID!,
+      clientSecret: process.env.APPLE_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: '/auth/signin',
+    signUp: '/auth/signup',
+  },
+})
+
+export { handler as GET, handler as POST }
