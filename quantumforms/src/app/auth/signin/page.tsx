0602b828d38ba82d352226baff52b1b19a545306
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 15:52:37 +0200

    [ADD]: Google icon

--- quantumforms/src/app/auth/signin/page.tsx
+++ quantumforms/src/app/auth/signin/page.tsx
@@ -3,9 +3,10 @@
 import { signIn } from 'next-auth/react'
 import { Button } from "@/components/ui/button"
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
-import { Github } from 'lucide-react'
+import { Github, Loader2 } from 'lucide-react'
 import { motion } from "framer-motion";
 import { useState } from 'react'
+import Image from 'next/image'
 
 export default function SignIn() {
   // Track loading state
@@ -68,14 +69,15 @@ export default function SignIn() {
           >
             {loading ?(
               <div className="flex items-center justify-center space-x-2">
-                <svg className="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
+                <Loader2 size={20} className='animate-spin' />
                 <span>Loading...</span>
               </div>
             ) : (
-              <img src="/google.svg" alt="Google" className="w-5 h-5 mr-2" />
+              <Image 
+                width={20} height={20}
+                src="/icons/google.png" 
+                alt="Google" className="w-5 h-5 mr-2" 
+              />
             )}
             {!loading ? 'Continue with Google' : null}
           </Button>
@@ -87,10 +89,7 @@ export default function SignIn() {
           >
             {loading ?(
               <div className="flex items-center justify-center space-x-2">
-                <svg className="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
+                <Loader2 size={20} className='animate-spin' />
                 <span>Loading...</span>
               </div>
             ) : (
