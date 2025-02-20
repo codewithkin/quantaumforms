504dfbdbfad0d831f12be815f0206c00a27943d6
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 23:51:59 +0200

    ADD: Implement QueryProvider component for managing React Query client

---
+++ quantumforms/src/context/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { ReactNode, useState } from 'react'
+
+function QueryProvider({children}: {children: ReactNode}) {
+    const [queryClient] = useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+        {children}
+    </QueryClientProvider>
+  )
+}
+
+export default QueryProvider
