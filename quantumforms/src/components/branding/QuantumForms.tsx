e74094b56f083bbcd3ede69c747297177b6a2dfe
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 16:36:20 +0200

    [DELETE]: Unused SVGs provided by NextJS

---
+++ quantumforms/src/components/branding/QuantumForms.tsx
@@ -0,0 +1,5 @@
+import { Scale3D } from "lucide-react";
+
+export default function QuantumForms () {
+    return <Scale3D size={20} className="bg-gradient-to-r bg-clip-text text-transparent from-purple-600 to-orange-500" />
+}
