ad46c2ba141dfa5bf0d976459e43aad0d2e234c1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-20 16:39:44 +0200

    [UPDATE]: Enhance branding colors and adjust icon size

--- quantumforms/src/components/branding/LogoPlusHeading.tsx
+++ quantumforms/src/components/branding/LogoPlusHeading.tsx
@@ -5,7 +5,7 @@ function LogoPlusHeading() {
   return (
     <article className="flex gap-2 items-center justify-center w-fit p-4">
         <QuantumForms />
-        <h2 className='text-xl font-semibold'>QuantumForms</h2>
+        <h2 className='text-xl font-semibold text-purple-600'>QuantumForms</h2>
     </article>
   )
 }
--- quantumforms/src/components/branding/QuantumForms.tsx
+++ quantumforms/src/components/branding/QuantumForms.tsx
@@ -1,5 +1,5 @@
 import { Scale3D } from "lucide-react";
 
 export default function QuantumForms () {
-    return <Scale3D size={20} className="bg-gradient-to-r bg-clip-text text-transparent from-purple-600 to-orange-500" />
+    return <Scale3D size={30} className="text-purple-600" />
 }
