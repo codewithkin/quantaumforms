f0a156c2abda8d9f41f5c5cc1be4be2dc790f971
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-21 15:04:58 +0200

    UPDATE: Refactor DraggableSection component to include form fields with labels and inputs, enhancing structure and usability

--- quantumforms/src/components/form-editor/DraggableSection.tsx
+++ quantumforms/src/components/form-editor/DraggableSection.tsx
@@ -1,28 +1,45 @@
 import { Form } from "@/types";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Textarea } from "@heroui/input";
 
-function DraggableSection({form}: {form: Form}) {
-    
+function DraggableSection({ form }: { form: Form }) {
   return (
     <article className="w-full h-full flex flex-col items-center justify-center text-center">
-        <Card className="cursor-grab px-8 py-4 md:min-w-[400px]">
-            <CardHeader>
-                <CardTitle className="text-2xl">{form.title}</CardTitle>
-                <CardDescription>{form.description}</CardDescription>
-            </CardHeader>
+      <Card className="cursor-grab px-8 py-4 md:min-w-[400px]">
+        <CardHeader>
+          <CardTitle className="text-2xl">{form.title}</CardTitle>
+          <CardDescription>{form.description}</CardDescription>
+        </CardHeader>
 
-            <CardContent>
-                {
-                    form.fields.length > 0 &&
-                    form.fields.map((field) => (
-                        <div key={field.id}>
-                            <h3>{field.label}</h3>
-                            <p>{field.type}</p>
-                        </div>
-                    ))
-                }
-            </CardContent>
-        </Card>
+        <CardContent>
+          <form className="w-full flex flex-col justify-start items-start">
+            {form.fields.length > 0 &&
+              form.fields.map((field) => {
+                return field.type !== "textarea" ?
+                 (
+                  <div key={field.id}>
+                    <Label>{field.label}</Label>
+                    <Input type={field.type} />
+                  </div>
+                ) :
+                (
+                  <div key={field.id}>
+                    <Label>{field.label}</Label>
+                    <Textarea />
+                  </div>
+                );
+              })}
+          </form>
+        </CardContent>
+      </Card>
     </article>
   );
 }
