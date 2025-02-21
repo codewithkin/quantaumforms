ae8462a9600759eadd08b24384c5322f1c5b0446
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-21 13:57:22 +0200

    UPDATE: Refactor DraggableSection component and add new Droppable component for improved drag-and-drop functionality

--- quantumforms/src/components/form-editor/DraggableSection.tsx
+++ quantumforms/src/components/form-editor/DraggableSection.tsx
@@ -1,35 +1,13 @@
-import React from "react";
-import { useDraggable, UseDraggableArguments } from "@dnd-kit/core";
-import { CSS } from "@dnd-kit/utilities";
 import { Form } from "@/types";
 import { Card } from "../ui/card";
 
-function DraggableSection({
-  form,
-  id,
-}: {
-  form: Form;
-  id: UseDraggableArguments["id"];
-}) {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id,
-  });
-  const style = {
-    // Outputs `translate3d(x, y, 0)`
-    transform: CSS.Translate.toString(transform),
-  };
-
+function DraggableSection({form}: {form: Form}) {
+    
   return (
     <article className="w-full h-full flex flex-col items-center justify-center text-center">
-      <Card
-        ref={setNodeRef}
-        style={style}
-        {...listeners}
-        {...attributes}
-        className="cursor-grab"
-      >
-        <h2>Helo</h2>
-      </Card>
+        <Card className="cursor-grab">
+            <h2>Helo</h2>
+        </Card>
     </article>
   );
 }
