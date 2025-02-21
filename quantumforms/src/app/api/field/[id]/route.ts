26bdfcc12d612c55d228327df880fd365ef9f32c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-21 15:22:58 +0200

    UPDATE: Implement DELETE API endpoint for fields with error handling and response messages

---
+++ quantumforms/src/app/api/field/[id]/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../prisma";
+
+// DELETE a particular field
+export async function DELETE (req: NextRequest, {params}: {params: {id: string}}) {
+    try {
+        const { id } = await params;
+
+        // Get the field id
+        const {fieldId} = await req.json();
+
+        // Find the form with the shareableLink equal to the id
+        const form = await prisma.form.findUnique({
+            where: {
+                shareableLink: id
+            }
+        })
+
+        if(!form) {
+            throw new Error("Form not found !");
+        }
+
+        // Delete the field
+        const deletedField = await prisma.field.delete({
+            where: {
+                id: fieldId
+            }
+        })
+
+        console.log("Successfully deleted field: ", deletedField);
+
+        return NextResponse.json(deletedField, {status: 200});
+    } catch (e) {
+        console.log("An error occured while deleting field: ", e);
+
+        return NextResponse.json({
+            message: "An error occured while deleting field",
+        }, {status: 500});
+    }
+}
