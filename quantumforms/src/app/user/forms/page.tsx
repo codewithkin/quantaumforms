b508529fb5bb349b99de741a8a22229f88342839
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-23 18:23:54 +0200

    [ADD]: Forms data fetch inside /forms

--- quantumforms/src/app/user/forms/page.tsx
+++ quantumforms/src/app/user/forms/page.tsx
@@ -1,12 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { Input } from "@heroui/input";
-import { DockIcon, ListCheck, Search, Table } from "lucide-react";
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { DockIcon, ListCheck, Loader, Loader2, Table } from "lucide-react";
+import axios from "axios";
 
 function Forms() {
+  // Fetch all the forms
+  const {data: forms, isLoading} = useQuery({
+    queryKey: ["forms"],
+    queryFn: async () => {
+      const res = await axios.get("/api/forms");
+
+      return res.data
+    }
+  })
+
+  console.log("FORMS IN /FORMS: ", forms);
+
   return (
     <article className="page w-full">
+      {/* Heading and view controls */}
       <article className="w-full justify-between items-center flex">
         <h2 className="text-2xl font-semibold">All Forms</h2>
 
@@ -50,6 +64,15 @@ function Forms() {
           </TooltipProvider>
         </article>
       </article>
+
+      {
+         forms ?
+        <h2>Data</h2> :
+        isLoading &&
+        <article className="w-full h-full flex flex-col justify-center items-center">
+          <Loader size={40} className="animate-spin" />
+        </article> 
+      }
     </article>
   );
 }
