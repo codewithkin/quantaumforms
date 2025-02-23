4d2c9dfe4e90afcaa81568f33c834454d4244029
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-23 14:23:10 +0200

    [ADD]: Empty Pages

---
+++ quantumforms/src/app/ai/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AI() {
+  return (
+    <article className="page">
+        <h2>Hello, AI !</h2>
+    </article>
+  )
+}
+
+export default AI
---
+++ quantumforms/src/app/user/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Analytics() {
+  return (
+    <article className="page">
+        <h2>Hello, Analytics !</h2>
+    </article>
+  )
+}
+
+export default Analytics
