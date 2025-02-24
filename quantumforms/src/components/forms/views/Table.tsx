0135143fccb1a7852f9b441bcf54c8834547d15c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-24 22:53:56 +0200

    UPDATE: Refactor FormTable component to utilize forms data and enhance cell rendering logic

--- quantumforms/src/components/forms/views/Table.tsx
+++ quantumforms/src/components/forms/views/Table.tsx
@@ -10,66 +10,8 @@ import {
   Chip,
   Tooltip,
 } from "@heroui/react";
-
-export const columns = [
-  {name: "NAME", uid: "name"},
-  {name: "ROLE", uid: "role"},
-  {name: "STATUS", uid: "status"},
-  {name: "ACTIONS", uid: "actions"},
-];
-
-export const users = [
-  {
-    id: 1,
-    name: "Tony Reichert",
-    role: "CEO",
-    team: "Management",
-    status: "active",
-    age: "29",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
-    email: "tony.reichert@example.com",
-  },
-  {
-    id: 2,
-    name: "Zoey Lang",
-    role: "Technical Lead",
-    team: "Development",
-    status: "paused",
-    age: "25",
-    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-    email: "zoey.lang@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Fisher",
-    role: "Senior Developer",
-    team: "Development",
-    status: "active",
-    age: "22",
-    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-    email: "jane.fisher@example.com",
-  },
-  {
-    id: 4,
-    name: "William Howard",
-    role: "Community Manager",
-    team: "Marketing",
-    status: "vacation",
-    age: "28",
-    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
-    email: "william.howard@example.com",
-  },
-  {
-    id: 5,
-    name: "Kristen Copper",
-    role: "Sales Manager",
-    team: "Sales",
-    status: "active",
-    age: "24",
-    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
-    email: "kristen.cooper@example.com",
-  },
-];
+import { Form } from "@/types";
+import ListViewShareableLinkBadge from "./ListViewShareableLinkBadge";
 
 export const EyeIcon = (props) => {
   return (
@@ -198,72 +140,59 @@ const statusColorMap = {
   vacation: "warning",
 };
 
-export default function FormTable() {
-  const renderCell = React.useCallback((user, columnKey) => {
-    const cellValue = user[columnKey];
+export default function FormTable({forms}: {forms: Form[]}) {
+ const columns = [
+        {name: "NAME", uid: "name"},
+        {name: "ROLE", uid: "role"},
+        {name: "STATUS", uid: "status"},
+        {name: "ACTIONS", uid: "actions"},
+      ];
+
+      // Add an "Actions" array to each of the forms
+      forms.forEach((form) => {
+        form.actions = [];
+      });
+
+  const renderCell = React.useCallback((form: Form, columnKey: any) => {
+    const cellValue = form[columnKey];
 
     switch (columnKey) {
-      case "name":
+      case "title":
         return (
-          <User
-            avatarProps={{radius: "lg", src: user.avatar}}
-            description={user.email}
-            name={cellValue}
-          >
-            {user.email}
-          </User>
+          <h2>{form.title}</h2>
         );
-      case "role":
+      case "shareableLInk":
         return (
-          <div className="flex flex-col">
-            <p className="text-bold text-sm capitalize">{cellValue}</p>
-            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
-          </div>
+            <ListViewShareableLinkBadge shareableLink={form.shareableLink} />
         );
-      case "status":
+      case "responses":
         return (
-          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
-            {cellValue}
-          </Chip>
+          <h2>{form.responses.length}</h2>
         );
       case "actions":
         return (
-          <div className="relative flex items-center gap-2">
-            <Tooltip content="Details">
-              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                <EyeIcon />
-              </span>
-            </Tooltip>
-            <Tooltip content="Edit user">
-              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                <EditIcon />
-              </span>
-            </Tooltip>
-            <Tooltip color="danger" content="Delete user">
-              <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                <DeleteIcon />
-              </span>
-            </Tooltip>
-          </div>
+          <h2>Actions here</h2>
         );
       default:
         return cellValue;
     }
   }, []);
 
+  
+
   return (
     <Table aria-label="Example table with custom cells">
       <TableHeader columns={columns}>
-        {(column) => (
+        {(column: any) => (
           <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
             {column.name}
           </TableColumn>
         )}
       </TableHeader>
-      <TableBody items={users}>
-        {(item) => (
+      <TableBody items={forms}>
+        {(item: any) => (
           <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+            {(columnKey: string) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
           </TableRow>
         )}
       </TableBody>
