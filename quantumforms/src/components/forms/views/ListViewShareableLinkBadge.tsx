82c7f3a2f7d8e2add5c6bcd89da3c13e51cb9b02
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-24 08:50:19 +0200

    [ADD]: Copy link to clipboard functionality

--- quantumforms/src/components/forms/views/ListViewShareableLinkBadge.tsx
+++ quantumforms/src/components/forms/views/ListViewShareableLinkBadge.tsx
@@ -1,12 +1,33 @@
 import { Badge } from '@/components/ui/badge'
+import { rootUrls } from '@/utils/rootUrls';
 import { Form } from '@prisma/client'
+import { Check, Link } from 'lucide-react';
 import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 function ListViewShareableLinkBadge({shareableLink}: {shareableLink: Form["shareableLink"]}) {
   const [copied, setCopied] = useState<boolean>(false);
 
+  const handleCopy = () => {
+    // Copy the link to the clipboard
+    navigator.clipboard.writeText(`${rootUrls.frontendUrl}/forms/${shareableLink}`);
+
+    toast("Copied form to clipboard")
+  }
+
   return (
-    <Badge variant={copied ? "outline" : "default"}>{shareableLink?.slice(0, 10)}</Badge>
+    <Badge 
+        onClick={handleCopy} 
+        variant={copied ? "outline" : "default"}
+        className='flex gap-2 items-center'
+    >
+        {
+            !copied ?
+            <Link size={14} /> :
+            <Check size={14} />
+        }
+        {shareableLink?.slice(0, 10)}
+    </Badge>
   )
 }
 
