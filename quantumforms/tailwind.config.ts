ad5b2f8a88b93f8db9508165bb9a80836f4fd413
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-24 22:47:36 +0200

    UPDATE: Replace AgGrid with Heroui Table component and update related configurations

--- quantumforms/tailwind.config.ts
+++ quantumforms/tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(avatar|input|form).js",
+    "./node_modules/@heroui/theme/dist/components/(avatar|input|table|form|checkbox|spacer).js",
   ],
   theme: {
     extend: {
