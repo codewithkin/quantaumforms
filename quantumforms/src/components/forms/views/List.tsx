d5f71fc81a62b6dbe6ad822368d1257f573c93be
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-24 09:01:15 +0200

    [ADD]; List view

--- quantumforms/src/components/forms/views/List.tsx
+++ quantumforms/src/components/forms/views/List.tsx
@@ -5,7 +5,7 @@ import ListViewShareableLinkBadge from './ListViewShareableLinkBadge';
 
 function FormListItem({forms}: {forms: Form[]}) {
   return (
-    <ul className='w-full flex flex-col gap-4'>
+    <ul className='w-full flex flex-col gap-4 overflow-hidden'>
       {
         forms.length > 0 ?
         forms.map((form: Form) => {
@@ -13,9 +13,18 @@ function FormListItem({forms}: {forms: Form[]}) {
 
           return (
             <li key={id} className="w-full justify-between flex items-center bg-white text-slate-800 shadow-md rounded-xl hover:cursor-pointer transition duration-300 p-4">
-              {title}
+              {/* Title and createdAt */}
+              <article className="flex gap-2 items-center">
+                <h4 className="font-semibold text-md">{title}</h4>
 
-              {createdAt.toString()}
+                <p className='text-slate-600 font-semibold'>{createdAt.toString()}</p>
+              </article>
+
+              {/* Primary and secondary colors
+              <article className="flex items-center gap-2">
+                <article className={`${primaryColor ? `bg-[${primaryColor}]` : "bg-orange-400"} w-8 h-8 rounded-full`}></article>
+                <article className={`${secondaryColor ? `bg-[${secondaryColor}]` : "bg-purple-400"} w-8 h-8 rounded-full`}></article>
+              </article> */}
 
               <ListViewShareableLinkBadge 
                 shareableLink={shareableLink} 
