17272df2f01ba519ef0af096362e6f0f9d01812b
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 05:34:05 +0200

    UPDATE: Add response filtering functionality to Forms component and clean up code

--- quantumforms/src/app/user/forms/page.tsx
+++ quantumforms/src/app/user/forms/page.tsx
@@ -13,7 +13,6 @@ import {
   DockIcon,
   ListCheck,
   Loader,
-  Loader2,
   Table,
 } from "lucide-react";
 import axios from "axios";
@@ -25,7 +24,7 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  DropdownMenuSeparator
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { Form } from "@/types";
 
@@ -35,18 +34,13 @@ function Forms() {
     queryKey: ["forms"],
     queryFn: async () => {
       const res = await axios.get("/api/forms");
-
       return res.data;
     },
   }) as { data: Form[]; isLoading: boolean };
 
-  console.log("FORMS IN /FORMS: ", forms);
-
   const [view, setView] = useState<"list" | "table" | "card">("list");
-
   const [filteredForms, setFilteredForms] = useState<Form[]>([]);
 
-  // Update filteredForms when forms are fetched
   useEffect(() => {
     if (forms) {
       setFilteredForms(forms);
@@ -55,13 +49,19 @@ function Forms() {
 
   const handleTimeFilter = (hours: number) => {
     if (!forms) return;
-
     const now = new Date().getTime();
     const newFilteredForms = forms.filter(
       (form) =>
-        new Date(form.createdAt).getTime() > now - hours * 60 * 60 * 1000,
+        new Date(form.createdAt).getTime() > now - hours * 60 * 60 * 1000
     );
+    setFilteredForms(newFilteredForms);
+  };
 
+  const handleResponseFilter = (min: number, max: number) => {
+    if (!forms) return;
+    const newFilteredForms = forms.filter(
+      (form) => form.responses.length >= min && form.responses.length <= max
+    );
     setFilteredForms(newFilteredForms);
   };
 
@@ -70,129 +70,57 @@ function Forms() {
       {/* Heading and view controls */}
       <article className="w-full justify-between items-center flex">
         <h2 className="text-2xl font-semibold">All Forms</h2>
-
-        {/* Views */}
-        <article className="flex gap-4 items-center justify-center">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                {/* List View */}
-                <Button
-                  onClick={() => setView("list")}
-                  size="icon"
-                  variant={view === "list" ? "default" : "outline"}
-                >
-                  <ListCheck size={30} strokeWidth={1.5} />
-                </Button>
-              </TooltipTrigger>
-
-              <TooltipContent side="top">List View</TooltipContent>
-            </Tooltip>
-
-            {/* Table View */}
-            <Tooltip>
-              <TooltipTrigger asChild>
-                {/* Table View */}
-                <Button
-                  onClick={() => setView("table")}
-                  size="icon"
-                  variant={view === "table" ? "default" : "outline"}
-                >
-                  <Table size={30} strokeWidth={1.5} />
-                </Button>
-              </TooltipTrigger>
-
-              <TooltipContent side="top">Table View</TooltipContent>
-            </Tooltip>
-
-            {/* Card View */}
-            <Tooltip>
-              <TooltipTrigger asChild>
-                {/* Card View */}
-                <Button
-                  onClick={() => setView("card")}
-                  size="icon"
-                  variant={view === "card" ? "default" : "outline"}
-                >
-                  <DockIcon size={30} strokeWidth={1.5} />
-                </Button>
-              </TooltipTrigger>
-
-              <TooltipContent side="top">Card View</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </article>
       </article>
 
       {/* Filters */}
       <article className="w-full justify-between items-center flex my-4">
-        <article className="flex gap-4 items-center justify-center"></article>
-
         <article className="flex gap-4 items-center justify-center">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button
-                className="flex gap-2 items-center"
-                variant="outline"
-                size="sm"
-              >
-                <Clock size={20} strokeWidth={1.5} />
-                Time
+              <Button className="flex gap-2 items-center" variant="outline" size="sm">
+                <Clock size={20} strokeWidth={1.5} /> Time
               </Button>
             </DropdownMenuTrigger>
-
             <DropdownMenuSeparator />
-
             <DropdownMenuContent>
-            <DropdownMenuGroup>
+              <DropdownMenuGroup>
                 <DropdownMenuItem>
-                  <Button
-                    variant="default"
-                    disabled={filteredForms.length !== 0}
-                    onClick={() => handleTimeFilter(15120)}
-                    size="sm"
-                  >
-                    <Brush size={20} strokeWidth={1.5} />
-                    Clear Filters
+                  <Button variant="default" onClick={() => setFilteredForms(forms || [])} size="sm">
+                    <Brush size={20} strokeWidth={1.5} /> Clear Filters
                   </Button>
                 </DropdownMenuItem>
               </DropdownMenuGroup>
               <DropdownMenuGroup>
                 <DropdownMenuItem>
-                  <Button
-                    variant="secondary"
-                    onClick={() => handleTimeFilter(24)}
-                    size="sm"
-                  >
-                    Last 24 hours
-                  </Button>
+                  <Button variant="secondary" onClick={() => handleTimeFilter(24)} size="sm">Last 24 hours</Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
-                  <Button
-                    variant="secondary"
-                    onClick={() => handleTimeFilter(168)}
-                    size="sm"
-                  >
-                    1 Week
-                  </Button>
+                  <Button variant="secondary" onClick={() => handleTimeFilter(168)} size="sm">1 Week</Button>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="flex gap-2 items-center" variant="outline" size="sm">
+                Responses
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuSeparator />
+            <DropdownMenuContent>
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Button variant="secondary" onClick={() => handleResponseFilter(0, 5)} size="sm">0 - 5 Responses</Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
-                  <Button
-                    variant="secondary"
-                    onClick={() => handleTimeFilter(5040)}
-                    size="sm"
-                  >
-                    1 Month
-                  </Button>
+                  <Button variant="secondary" onClick={() => handleResponseFilter(6, 20)} size="sm">6 - 20 Responses</Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem>
-                  <Button
-                    variant="secondary"
-                    onClick={() => handleTimeFilter(15120)}
-                    size="sm"
-                  >
-                    3 Months
-                  </Button>
+                  <Button variant="secondary" onClick={() => handleResponseFilter(21, 50)} size="sm">21 - 50 Responses</Button>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button variant="secondary" onClick={() => handleResponseFilter(51, 100)} size="sm">51 - 100 Responses</Button>
                 </DropdownMenuItem>
               </DropdownMenuGroup>
             </DropdownMenuContent>
