e83bc7bbcded2ae6c193e15fac82ae5a68d8b8bb
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 04:39:14 +0200

    UPDATE: Add animation effects to FormCards component for enhanced visual appeal

--- quantumforms/src/components/forms/views/Card.tsx
+++ quantumforms/src/components/forms/views/Card.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Form } from '@/types'
 import { Pencil } from 'lucide-react';
 import Link from 'next/link';
+import {motion} from "framer-motion";
 import CardsViewMoreOptions from './CardsViewMoreOptions';
 
 function FormCards({forms}: {forms: Form[]}) {
@@ -9,10 +10,25 @@ function FormCards({forms}: {forms: Form[]}) {
     <article className="grid md:grid-cols-4 sm:grid-cols-2 items-center md:gap-8 gap-4">
         {
             forms && forms.length > 0 &&
-            forms.map((form: Form) => {
+            forms.map((form: Form, index: number) => {
                 const {id, title, createdAt, shareableLink} = form;
                 return (
-                    <article key={id} className={`hover:cursor-pointer grid min-h-[400px] p-4 rounded-xl text-white bg-gradient-to-tr from-orange-300 to-purple-600 w-full`}>
+                    <motion.article 
+                    initial={{
+
+                        opacity: 0,
+                        x: 200
+                      }}
+      
+                      animate={{
+                        opacity: 1,
+                        x: 1
+                      }}
+      
+                      transition={{
+                        delay: index * 0.1
+                      }}
+                    key={id} className={`hover:cursor-pointer grid min-h-[400px] p-4 rounded-xl text-white bg-gradient-to-tr from-orange-300 to-purple-600 w-full`}>
                         <article>
                             <h3 className='text-xl font-semibold'>{title}</h3>
                             <p className='font-semibold'>{createdAt}</p>
@@ -29,7 +45,7 @@ function FormCards({forms}: {forms: Form[]}) {
                             </Button>
                             <CardsViewMoreOptions form={form} />
                         </article>
-                    </article>
+                    </motion.article>
                 )
             })
         }
